5dfe9e398a0addd140bf9ab3139fb4de46678e86
edsonmendieta <emendietaj@gmail.com>
2016-09-30 11:57:38 -0700

    edited container classes to fit this app, and added a working CORS request to the Twitch API.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,17 @@
 console.log('hello!');
+
+
+// my Twitch client ID : mlrx1e94dg7yus5yqm26lwpyxrg9j9x
+
+ var xhr = new XMLHttpRequest();
+ xhr.open('GET', 'https://api.twitch.tv/kraken/streams/kinggothalion/?client_id=mlrx1e94dg7yus5yqm26lwpyxrg9j9x');
+ xhr.onreadystatechange = twitchInfo;
+ xhr.send();
+
+ function twitchInfo() {
+
+     if(this.readyState == 4 && this.status == 200) {
+
+         console.log(JSON.parse(this.response));
+     }
+ }
